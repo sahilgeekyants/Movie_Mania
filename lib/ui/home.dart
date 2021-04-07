bc3a1bf3af2f64a63027876c834797998ef0189e
Sahil Garg <sahilg@geekyants.com>
2021-04-08 02:58:51 +0530

    Added UI for home screen AppBar

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/services.dart';
+import 'package:movie_mania/utils/scale_config.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -10,6 +12,17 @@ class _HomeState extends State<Home> {
   @override
   void initState() {
     super.initState();
+    // orfixientation for now
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    //save values of scale config
+    SizeScaleConfig().calculateScaleRatios(context);
   }
 
   @override
@@ -17,8 +30,56 @@ class _HomeState extends State<Home> {
     super.dispose();
   }
 
+  final SizeScaleConfig scaleConfig = SizeScaleConfig();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        leading: IconButton(
+          padding: EdgeInsets.only(left: scaleConfig.scaleWidth(35)),
+          iconSize: scaleConfig.scaleWidth(45),
+          icon: Icon(
+            // Icons.movie_creation_rounded,
+            // Icons.theaters_outlined,
+            Icons.movie_filter_outlined,
+            color: Colors.black87,
+          ),
+          onPressed: () {},
+        ),
+        centerTitle: false,
+        title: Padding(
+          padding: EdgeInsets.only(left: scaleConfig.scaleWidth(15)),
+          child: Text(
+            'Movie Mania',
+            style: TextStyle(
+              color: Colors.black87,
+              fontSize: scaleConfig.scaleWidth(24),
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        actions: <Widget>[
+          IconButton(
+            padding: EdgeInsets.only(right: scaleConfig.scaleWidth(35)),
+            iconSize: scaleConfig.scaleWidth(35),
+            icon: Icon(
+              Icons.search,
+              color: Colors.black87,
+            ),
+            onPressed: () {
+              print('Search button pressed');
+            },
+          ),
+        ],
+      ),
+      body: SafeArea(
+        top: false,
+        bottom: false,
+        child: Container(),
+      ),
+    );
   }
 }
