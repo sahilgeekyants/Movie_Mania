4eceb07472ea14bffb7300cf7aab1a1b578e8dcf
Sahil Garg <sahilg@geekyants.com>
2021-04-08 04:12:59 +0530

    Added basic UI for bottom navigation bar in home screen

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -75,6 +75,72 @@ class _HomeState extends State<Home> {
           ),
         ],
       ),
+      bottomNavigationBar: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+                topRight: Radius.circular(30), topLeft: Radius.circular(30)),
+            boxShadow: [
+              BoxShadow(color: Colors.black26, spreadRadius: 0, blurRadius: 5),
+            ],
+          ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(30.0),
+              topRight: Radius.circular(30.0),
+            ),
+            child: BottomNavigationBar(
+              type: BottomNavigationBarType.fixed,
+              currentIndex: 0,
+              // fixedColor: Colors.black87,
+              // unselectedItemColor: Colors.grey,
+              iconSize: scaleConfig.scaleWidth(30),
+              showSelectedLabels: false,
+              showUnselectedLabels: false,
+              selectedIconTheme: IconThemeData(
+                color: Colors.black87,
+                size: scaleConfig.scaleWidth(38),
+              ),
+              unselectedIconTheme: IconThemeData(
+                color: Colors.grey,
+                size: scaleConfig.scaleWidth(34),
+              ),
+              items: <BottomNavigationBarItem>[
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.description_outlined),
+                  // activeIcon: Container(
+                  //   decoration: BoxDecoration(
+                  //     borderRadius: BorderRadius.all(Radius.circular(30)),
+                  //     color: Colors.grey[300],
+                  //   ),
+                  //   child: Row(
+                  //     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  //     mainAxisSize: MainAxisSize.min,
+                  //     children: <Widget>[
+                  //       Icon(Icons.description_outlined),
+                  //       Text(
+                  //         'Feed',
+                  //         style: TextStyle(
+                  //           color: Colors.black87,
+                  //           fontSize: scaleConfig.scaleWidth(17),
+                  //           fontWeight: FontWeight.bold,
+                  //         ),
+                  //       ),
+                  //     ],
+                  //   ),
+                  // ),
+                  label: 'Feed',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.bookmark_outline_rounded),
+                  label: 'Bookmark',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.person_outline),
+                  label: 'Person',
+                ),
+              ],
+            ),
+          )),
       body: SafeArea(
         top: false,
         bottom: false,
