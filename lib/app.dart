97a7ddb81cfbcb7e7f91775d946d97fcdf1fbaf4
Sahil Garg <sahilg@geekyants.com>
2021-04-08 02:45:18 +0530

    Updated app & main file

--- lib/app.dart
+++ lib/app.dart
@@ -1,12 +1,42 @@
 import 'package:flutter/material.dart';
+import 'ui/home.dart';
 
-class App extends StatelessWidget {
+//Navigation key
+final GlobalKey<NavigatorState> navigatorKey = new GlobalKey<NavigatorState>();
+
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  static const MaterialColor white = MaterialColor(
+    0xFFFFFFFF,
+    const <int, Color>{
+      50: const Color(0xFFFFFFFF),
+      100: const Color(0xFFFFFFFF),
+      200: const Color(0xFFFFFFFF),
+      300: const Color(0xFFFFFFFF),
+      400: const Color(0xFFFFFFFF),
+      500: const Color(0xFFFFFFFF),
+      600: const Color(0xFFFFFFFF),
+      700: const Color(0xFFFFFFFF),
+      800: const Color(0xFFFFFFFF),
+      900: const Color(0xFFFFFFFF),
+    },
+  );
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark(),
+      navigatorKey: navigatorKey,
+      debugShowCheckedModeBanner: false,
+      title: "Movie Mania",
+      theme: ThemeData(
+        fontFamily: 'Roboto',
+        primarySwatch: white,
+      ),
       home: Scaffold(
-        body: Container(),
+        body: Home(),
       ),
     );
   }
