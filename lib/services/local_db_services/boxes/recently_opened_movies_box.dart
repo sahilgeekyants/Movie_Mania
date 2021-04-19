2b765df440b5ab4dd63156d0c28e6db39f3a6603
Sahil Garg <sahilg@geekyants.com>
2021-04-20 04:21:29 +0530

    Added 2 new boxes in hive for local_details & rated_movies data

--- lib/services/local_db_services/boxes/recently_opened_movies_box.dart
+++ lib/services/local_db_services/boxes/recently_opened_movies_box.dart
@@ -4,44 +4,43 @@ import 'package:movie_mania/models/db_data_models/movie_data_model.dart';
 class RecentlyOpenedMoviesBox {
   //
   static final String boxName = "recently_opened_movie";
-  static Box<MovieDataModel> _recentOpenedMovieBox;
+  static Box<MovieDataModel> _box;
   //
   static Future openBox() async {
-    _recentOpenedMovieBox = await Hive.openBox<MovieDataModel>(boxName);
+    _box = await Hive.openBox<MovieDataModel>(boxName);
   }
 
   static bool hasData() {
-    if (_recentOpenedMovieBox == null) {
+    if (_box == null) {
       openBox();
     }
-    return _recentOpenedMovieBox.isNotEmpty;
+    return _box.isNotEmpty;
   }
 
   static Future addMovie(MovieDataModel model) async {
-    if (_recentOpenedMovieBox == null) {
+    if (_box == null) {
       await openBox();
     }
-    // await _recentOpenedMovieBox.delete(model.id);
-    // if (!_recentOpenedMovieBox.containsKey(model.id)) {
-    await _recentOpenedMovieBox.put(model.id, model);
+    // await _box.delete(model.id);
+    // if (!_box.containsKey(model.id)) {
+    await _box.put(model.id, model);
     // }
   }
 
   static MovieDataModel getMovie(int id) {
-    if (_recentOpenedMovieBox == null) {
+    if (_box == null) {
       openBox();
     }
-    MovieDataModel _model = _recentOpenedMovieBox.get(id);
+    MovieDataModel _model = _box.get(id);
     return _model;
   }
 
   static Map<int, MovieDataModel> getAllMoviesSortedByTime() {
-    if (_recentOpenedMovieBox == null) {
+    if (_box == null) {
       openBox();
     }
-    // _recentOpenedMovieBox.values.toList();
-    Map<int, MovieDataModel> _movies =
-        _recentOpenedMovieBox.toMap().map((key, value) {
+    // _box.values.toList();
+    Map<int, MovieDataModel> _movies = _box.toMap().map((key, value) {
       int date = value.lastOpened.microsecondsSinceEpoch;
       return MapEntry(date, value);
     });
