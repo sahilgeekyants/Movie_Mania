f559d1703152eccc383296ebef7e7ffdb7eb7fc4
Sahil Garg <sahilg@geekyants.com>
2021-05-04 11:18:19 +0530

    Added the search screen to the router

--- lib/router.dart
+++ lib/router.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:movie_mania/ui/splash_screen/splash.dart';
 import 'ui/home_screen/home.dart';
 import 'ui/movie_detail_screen/movie_detail.dart';
+import 'ui/search_screen/search.dart';
 
 class MyRouter {
   // static const String homeRoute = '/';
@@ -20,6 +21,8 @@ class MyRouter {
             movieModel: args.movieModel,
           ),
         );
+      case Search.routeName:
+        return MaterialPageRoute(builder: (context) => Search());
       default:
         return MaterialPageRoute(builder: (context) {
           return Scaffold(
