239ef5282afd530d8cc7f7a85953f3afbc1128d8
Sahil Garg <sahilg@geekyants.com>
2021-05-11 15:07:33 +0530

    Completed basic UI for seach screen with data linking and used rating stars as well

--- lib/ui/search_screen/search.dart
+++ lib/ui/search_screen/search.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:movie_mania/services/config/config.dart';
 import 'package:movie_mania/utils/scale_config.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class Search extends StatefulWidget {
   static const String routeName = '/search';
@@ -18,8 +19,8 @@ class _SearchState extends State<Search> {
   @override
   Widget build(BuildContext context) {
     double screenWidth = SizeScaleConfig.screenWidth;
-    double imageheight = SizeScaleConfig.screenWidth * 0.41;
-    double imageWidth = SizeScaleConfig.screenWidth * 0.27;
+    double imageheight = SizeScaleConfig.screenWidth * 0.45;
+    double imageWidth = SizeScaleConfig.screenWidth * 0.3;
     double imageHorizontalPadding = SizeScaleConfig.screenWidth * 0.05;
     double cardHorizontalMargin = scaleConfig.scaleWidth(40);
     double cardTopMargin = scaleConfig.scaleWidth(25);
@@ -32,7 +33,6 @@ class _SearchState extends State<Search> {
           child: ListView.builder(
             itemCount: 5,
             itemBuilder: (context, id) {
-              //path - /xGuOF1T3WmPsAcQEQJfnG7Ud9f8.jpg
               String _posterPath =
                   Config.baseImageUrl + "/xGuOF1T3WmPsAcQEQJfnG7Ud9f8.jpg";
               return Container(
@@ -42,7 +42,7 @@ class _SearchState extends State<Search> {
                   left: cardHorizontalMargin / 2,
                   top: cardTopMargin,
                 ),
-                height: screenWidth * 0.45,
+                height: screenWidth * 0.5,
                 width: screenWidth - cardHorizontalMargin,
                 child: Stack(
                   children: [
@@ -52,7 +52,7 @@ class _SearchState extends State<Search> {
                       bottom: 0,
                       child: Container(
                         // color: Colors.red,
-                        height: screenWidth * 0.4,
+                        height: screenWidth * 0.45,
                         width: screenWidth - cardHorizontalMargin,
                         child: Stack(
                           children: [
@@ -67,7 +67,7 @@ class _SearchState extends State<Search> {
                                   borderRadius:
                                       BorderRadius.all(Radius.circular(3)),
                                 ),
-                                height: screenWidth * 0.33,
+                                height: screenWidth * 0.4,
                                 width: screenWidth - cardHorizontalMargin,
                                 child: Container(
                                   margin: EdgeInsets.only(
@@ -84,7 +84,99 @@ class _SearchState extends State<Search> {
                                       //Detail Body
                                       Expanded(
                                         child: Container(
-                                          color: Colors.orange,
+                                          // color: Colors.orange,
+                                          child: Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              SizedBox(
+                                                  height: scaleConfig
+                                                      .scaleWidth(5)),
+                                              Text(
+                                                "Berserker",
+                                                maxLines: 2,
+                                                overflow: TextOverflow.ellipsis,
+                                                style: TextStyle(
+                                                  color: Colors.black,
+                                                  fontWeight: FontWeight.bold,
+                                                  fontSize: scaleConfig
+                                                      .scaleWidth(22),
+                                                ),
+                                              ),
+                                              SizedBox(
+                                                  height: scaleConfig
+                                                      .scaleWidth(4)),
+                                              Row(
+                                                mainAxisAlignment:
+                                                    MainAxisAlignment.start,
+                                                crossAxisAlignment:
+                                                    CrossAxisAlignment.center,
+                                                children: [
+                                                  RatingBarIndicator(
+                                                    rating: 8.7 / 2,
+                                                    itemBuilder:
+                                                        (context, index) =>
+                                                            Icon(
+                                                      Icons.star,
+                                                      color: Colors.amber,
+                                                    ),
+                                                    itemCount: 5,
+                                                    itemSize: scaleConfig
+                                                        .scaleWidth(22),
+                                                    unratedColor: Colors.white,
+                                                  ),
+                                                  Container(
+                                                    padding: EdgeInsets.only(
+                                                      left: scaleConfig
+                                                          .scaleWidth(8),
+                                                      top: scaleConfig
+                                                          .scaleWidth(4),
+                                                    ),
+                                                    child: Text(
+                                                      "8.7",
+                                                      style: TextStyle(
+                                                        color: Colors
+                                                            .amberAccent[700],
+                                                        fontWeight:
+                                                            FontWeight.w700,
+                                                        fontSize: scaleConfig
+                                                            .scaleWidth(18),
+                                                      ),
+                                                    ),
+                                                  ),
+                                                ],
+                                              ),
+                                              Expanded(child: Container()),
+                                              Text(
+                                                "Action Thrill Sci-fi",
+                                                maxLines: 1,
+                                                overflow: TextOverflow.ellipsis,
+                                                style: TextStyle(
+                                                  color: Colors.black54,
+                                                  fontWeight: FontWeight.w600,
+                                                  fontSize: scaleConfig
+                                                      .scaleWidth(18),
+                                                ),
+                                              ),
+                                              SizedBox(
+                                                  height: scaleConfig
+                                                      .scaleWidth(4)),
+                                              Text(
+                                                "Director: John Walts",
+                                                maxLines: 1,
+                                                overflow: TextOverflow.ellipsis,
+                                                style: TextStyle(
+                                                  color: Colors.black54,
+                                                  fontWeight: FontWeight.w600,
+                                                  fontSize: scaleConfig
+                                                      .scaleWidth(18),
+                                                ),
+                                              ),
+                                              SizedBox(
+                                                  height: scaleConfig
+                                                      .scaleWidth(10)),
+                                            ],
+                                          ),
                                         ),
                                       ),
                                     ],
@@ -101,8 +193,7 @@ class _SearchState extends State<Search> {
                                   // state.isBookmarked ?
                                   // Icons.bookmark_outlined,
                                   Icons.bookmark_outline_rounded,
-                                  // : Icons.bookmark_outline_rounded,
-                                  color: Colors.lightBlue,
+                                  color: Colors.black87,
                                   size: scaleConfig.scaleWidth(45),
                                 ),
                                 onPressed: () {
@@ -120,7 +211,7 @@ class _SearchState extends State<Search> {
                       child: Container(
                         decoration: BoxDecoration(
                           color: Colors.grey,
-                          borderRadius: BorderRadius.all(Radius.circular(3)),
+                          borderRadius: BorderRadius.all(Radius.circular(5)),
                           image: DecorationImage(
                             image: NetworkImage(_posterPath),
                             fit: BoxFit.cover,
